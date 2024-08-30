c0568b127bf59ebe9e209346f4f86de3036a91a4
Benjhy <benjhy1504@gmail.com>
2024-08-30 12:20:37 -0500

    Diseño base de BottomBar

--- app/src/main/java/com/faustino/faustitalk/Navigation/MainScreen.kt
+++ app/src/main/java/com/faustino/faustitalk/Navigation/MainScreen.kt
@@ -2,29 +2,40 @@ package com.faustino.faustitalk.Navigation
 
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.padding
+
+
+
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Icon
-import androidx.compose.material3.NavigationBar
-import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.IconButton
+
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
-import androidx.navigation.NavDestination
-import androidx.navigation.NavDestination.Companion.hierarchy
-import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.compose.ui.Alignment
 import androidx.navigation.NavHostController
-import androidx.navigation.compose.currentBackStackEntryAsState
+
 import androidx.navigation.compose.rememberNavController
 import com.faustino.faustitalk.Navigation.Graphs.MainNavGraph
 import com.faustino.faustitalk.View.Auth.ViewModel.AuthViewModel
+import com.faustino.faustitalk.ui.theme.Dark900
+import com.faustino.faustitalk.ui.theme.Green300
 
 @Composable
 fun MainScreen(navHostController: NavHostController = rememberNavController(),authViewModel: AuthViewModel) {
 
     Scaffold (
-        bottomBar = {BottonBar(navHostController)},
+        bottomBar = { BottomBarCustom(navController = navHostController)},
     ){ paddingValues ->
         var modifier = Modifier.padding(paddingValues)
         MainNavGraph(navHostController)
@@ -32,8 +43,51 @@ fun MainScreen(navHostController: NavHostController = rememberNavController(),au
 
 }
 
+@Composable
+fun BottomBarCustom(navController: NavController) {
+    val  selected = remember {
+        mutableStateOf( BottonBarScreen.Home.icon )
+    }
+
+    BottomAppBar (
+        containerColor = Dark900,
+        actions = {
+            IconBottomCustom(selected,BottonBarScreen.Home,navController)
+            IconBottomCustom(selected,BottonBarScreen.Search,navController)
+            IconBottomCustom(selected,BottonBarScreen.Post,navController)
+            IconBottomCustom(selected,BottonBarScreen.Inbox,navController)
+            IconBottomCustom(selected,BottonBarScreen.Profile,navController)
+        }
+    )
+}
+
+@Composable
+fun RowScope.IconBottomCustom(
+    selected: MutableState<ImageVector>,
+    screen: BottonBarScreen,
+    navController: NavController
+) {
+    IconButton(
+
+        onClick = {
+            selected.value = screen.icon
+            navController.navigate( screen.route ){
+                popUpTo(0)
+            }
+        },
+        modifier = Modifier.weight(1f),
+    ) {
+        Icon(
+            imageVector = screen.icon,
+            contentDescription = "IconBar",
+            modifier = Modifier.size(30.dp),
+            tint = if (selected.value == screen.icon)  Green300 else Color.White
+        )
+    }
+}
 
 
+/*
 @Composable
 fun BottonBar(navController: NavController) {
     val screen = listOf(
@@ -77,4 +131,4 @@ fun RowScope.AddItems(
         } },
         icon = { Icon(imageVector = pages.icon, contentDescription = "icon_image") })
 
-}
+}*/
--- app/src/main/java/com/faustino/faustitalk/View/Register_Profile/RP4Screen.kt
+++ app/src/main/java/com/faustino/faustitalk/View/Register_Profile/RP4Screen.kt
@@ -15,15 +15,13 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.grid.itemsIndexed
+
 import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.foundation.shape.CircleShape
+
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.ShapeDefaults
+
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
