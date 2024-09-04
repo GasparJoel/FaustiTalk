798d1f5c07aa26ad146ca0dd720070f36b43cc64
Benjhy <benjhy1504@gmail.com>
2024-09-04 15:45:15 -0500

    correcion de error de duplicacte classes 2 (ahora si)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,7 +64,6 @@ dependencies {
     implementation(libs.androidx.material3)
     implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.runtime.livedata)
-    implementation(libs.androidx.room.compiler)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
