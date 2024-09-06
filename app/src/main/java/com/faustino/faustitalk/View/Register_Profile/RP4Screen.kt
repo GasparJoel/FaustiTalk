2b2bcafc7c322782cd22890cbf8ba93062817885
Joel Gt <0332201030@unjfsc.edu.pe>
2024-09-05 19:36:29 -0500

    Integracion de dos columnas con el uso de Gridcells para los buttons

--- app/src/main/java/com/faustino/faustitalk/View/Register_Profile/RP4Screen.kt
+++ app/src/main/java/com/faustino/faustitalk/View/Register_Profile/RP4Screen.kt
@@ -1,186 +1,89 @@
 package com.faustino.faustitalk.View.Register_Profile
 
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.gestures.FlingBehavior
-import androidx.compose.foundation.gestures.ScrollableDefaults
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.ExperimentalLayoutApi
-import androidx.compose.foundation.layout.FlowRow
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.itemsIndexed
-import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
-import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
-import androidx.compose.foundation.lazy.staggeredgrid.itemsIndexed
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.ShapeDefaults
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.faustino.faustitalk.R
 import com.faustino.faustitalk.View.Components.Butons.Btn_SiguienteGreen
-import com.faustino.faustitalk.View.Components.Fondos.BgFondoCuestion
-import com.faustino.faustitalk.View.Components.Items.ItemSelectInterest
 import com.faustino.faustitalk.View.Components.Texts.CustomTextCuestions
-import com.faustino.faustitalk.View.Components.Texts.Text300
 import com.faustino.faustitalk.ui.theme.Dark900
 import com.faustino.faustitalk.ui.theme.Green300
+import com.faustino.faustitalk.View.Components.Items.ItemSelectInterest
 
 @OptIn(ExperimentalLayoutApi::class)
 @Preview
 @Composable
 fun RP4Screen(modifier: Modifier = Modifier, finishClick: () -> Unit = {}) {
-
-   // BgFondoCuestion()
     Column(
         horizontalAlignment = Alignment.Start,
         modifier = modifier
             .fillMaxSize()
             .padding(horizontal = 32.dp),
     ) {
-
         Spacer(modifier = Modifier.height(26.dp))
         CustomTextCuestions(titulo = "Elige tus Intereses")
         Spacer(modifier = Modifier.height(10.dp))
-        LazyColumn(
+
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(2),
             modifier = Modifier
                 .weight(1f)
                 .fillMaxWidth(),
-            contentPadding = PaddingValues(vertical = 16.dp),
-            // columns = GridCells.Fixed(2)
+            contentPadding = PaddingValues(vertical = 16.dp, horizontal = 0.dp),
+            horizontalArrangement = Arrangement.spacedBy(8.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp)
         ) {
-            itemsIndexed(items) { index, item ->
-                Box(contentAlignment = Alignment.CenterStart){
-                    GridItemSelect(item = item)
-
-                }
-
+            itemsIndexed(items) { _, item ->
+                GridItemSelect(item = item)
             }
         }
-        /*
-        FlowRow(
-            modifier = Modifier
-                .weight(1f)
-                .fillMaxWidth(),
-//contentPadding = PaddingValues(vertical = 16.dp),
-// columns = GridCells.Fixed(2)
-        ) {
-            items.forEach { item ->
-                //Box(contentAlignment = Alignment.CenterStart){
-                GridItemSelect(item = item)
-                //   }
-
-            }
-        }*/
         Spacer(modifier = Modifier.height(20.dp))
-        Btn_SiguienteGreen(title = "Continuar" , onClick = { finishClick()},enabled = true)
+        Btn_SiguienteGreen(title = "Continuar", onClick = finishClick, enabled = true)
         Spacer(modifier = Modifier.height(50.dp))
     }
-
-
-
 }
-/*
-FlowRow(
-modifier = Modifier
-.weight(1f)
-.fillMaxWidth(),
-//contentPadding = PaddingValues(vertical = 16.dp),
-// columns = GridCells.Fixed(2)
-) {
-    items.forEach { item ->
-        //Box(contentAlignment = Alignment.CenterStart){
-        GridItemSelect(item = item)
-        //   }
-
-    }
-}
-*/
 
 @Composable
-fun GridItemSelect(item:ItemSelectInterest) {
-
+fun GridItemSelect(item: ItemSelectInterest) {
     var hiddenIconSelect by remember { mutableStateOf(false) }
 
-    var colorButton = if (hiddenIconSelect) {
-        Green300
-    }else{
-        Color.White.copy(0.09f)
+    val colorButton = if (hiddenIconSelect) Green300 else Color.White.copy(0.09f)
+    val colorText = if (hiddenIconSelect) Dark900 else Color.White
+
+    Button(
+        shape = RoundedCornerShape(30.dp),
+        colors = ButtonDefaults.buttonColors(containerColor = colorButton),
+        modifier = Modifier
+            .padding(4.dp)
+            .fillMaxWidth(),
+        onClick = { hiddenIconSelect = !hiddenIconSelect }
+    ) {
+        Text(modifier = Modifier.padding(end = 4.dp), text = item.emoji, fontSize = 17.sp)
+        Text(
+            text = item.text,
+            fontSize = 14.sp,
+            color = colorText,
+            textAlign = TextAlign.Center
+        )
     }
-    var colorText = if (hiddenIconSelect) {
-        Dark900
-    }else{
-        Color.White
-    }
-
-        Button (
-            shape = RoundedCornerShape(30.dp),
-            colors = ButtonDefaults.buttonColors(
-                containerColor = colorButton
-            ),
-            modifier = Modifier.padding(horizontal = 2.dp)
-
-            ,
-            onClick = {
-                hiddenIconSelect = !hiddenIconSelect
-            },
-
-            )
-        {/*
-            //Box( contentAlignment = Alignment.CenterStart) {
-                if(hiddenIconSelect){
-                    Image(painter = painterResource(id = R.drawable.ic_checkselect), contentDescription = "" , modifier = Modifier.padding(start = 2.dp, end = 6.dp))
-                }else{
-                }*/
-                    Text(modifier = Modifier.padding(end = 4.dp), text = item.emoji , fontSize = 20.sp, )
-
-                Text(
-                    modifier = Modifier
-                        //.padding(start = 28.dp)
-                        ,
-                    text = item.text ,
-                    fontSize = 20.sp,
-                    color = colorText,
-                    textAlign = TextAlign.Center
-
-                )
-            //}
-        }
-
-
 }
 
 private val items = listOf(
-    ItemSelectInterest("⚽","Fútbol"),
+    ItemSelectInterest("⚽", "Fútbol"),
     ItemSelectInterest("🎨", "Arte"),
-    ItemSelectInterest("🎮", "Videojuegos"),
+    ItemSelectInterest("🎮", "Video juegos"),
     ItemSelectInterest("🎵", "Música"),
     ItemSelectInterest("📚", "Lectura"),
     ItemSelectInterest("🏀", "Baloncesto"),
@@ -192,26 +95,11 @@ private val items = listOf(
     ItemSelectInterest("💻", "Tecnología"),
     ItemSelectInterest("🎤", "Cantar"),
     ItemSelectInterest("🏋️‍♂️", "Gimnasio"),
-    ItemSelectInterest("💻", "Tecnología"),
     ItemSelectInterest("📸", "Fotografía"),
-    ItemSelectInterest("⚽", "Fútbol"),
-    ItemSelectInterest("🎨", "Arte"),
-    ItemSelectInterest("🎮", "Videojuegos"),
-    ItemSelectInterest("🎵", "Música"),
-    ItemSelectInterest("📚", "Lectura"),
-    ItemSelectInterest("🏀", "Baloncesto"),
-    ItemSelectInterest("🍳", "Cocina"),
-    ItemSelectInterest("✈️", "Viajar"),
-    ItemSelectInterest("🎬", "Cine"),
-    ItemSelectInterest("🚴‍♂️", "Ciclismo"),
-    ItemSelectInterest("🎤", "Cantar"),
     ItemSelectInterest("🧘‍♂️", "Yoga"),
-    ItemSelectInterest("💻", "Tecnología"),
-    ItemSelectInterest("📸", "Fotografía"),
     ItemSelectInterest("🏊‍♂️", "Natación"),
     ItemSelectInterest("🎭", "Teatro"),
     ItemSelectInterest("🎹", "Piano"),
-    ItemSelectInterest("🏋️‍♂️", "Gimnasio"),
     ItemSelectInterest("🏕️", "Camping"),
     ItemSelectInterest("📈", "Inversiones"),
     ItemSelectInterest("🎣", "Pesca"),
@@ -227,8 +115,6 @@ private val items = listOf(
     ItemSelectInterest("📖", "Escritura"),
     ItemSelectInterest("🏇", "Equitación"),
     ItemSelectInterest("🏹", "Tiro con arco"),
-    ItemSelectInterest("🕹️", "Juegos retro"),
     ItemSelectInterest("🎧", "Podcasts"),
     ItemSelectInterest("🎻", "Violín")
 )
-
